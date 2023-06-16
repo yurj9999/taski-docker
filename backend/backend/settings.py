b100929f34b3b6a2d77a4561713ee8b6a10beafc
Юрий Туголуков <yurj9999@yandex.ru>
2023-06-16 13:32:10 +0300

    g

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,10 +64,13 @@ DATABASES = {
         # Меняем настройку Django: теперь для работы будет использоваться
         # бэкенд postgresql
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
+        #'NAME': os.getenv('POSTGRES_DB', 'django'),
+        #'USER': os.getenv('POSTGRES_USER', 'django'),
+        'NAME': os.getenv('POSTGRES_DB', ''),
+        'USER': os.getenv('POSTGRES_USER', ''),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
+        #'PORT': os.getenv('DB_PORT', 5432)
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
