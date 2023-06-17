e92ad57538f71dfb0ae2f8e0db963249f045be48
Юрий Туголуков <yurj9999@yandex.ru>
2023-06-17 20:59:15 +0300

    ddd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['taski-999.ddns.net', '127.0.0.1', 'localhost']
 
 
 # Application definition
